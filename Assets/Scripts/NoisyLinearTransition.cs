bb6b92e03e38337dcfdf441c162b818f2a8c35f7
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-10-26 17:20:17 +0200

    Added a Transition subclass for handling linear transitions with noise (for screen shake)

---
+++ Assets/Scripts/NoisyLinearTransition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class NoisyLinearTransition : Transition
+{
+    // The noise's maximum value
+    public float NoiseMax
+    {
+        private get;
+        set;
+    } = 1f;
+
+    // The noise's minimum value
+    public float NoiseMin
+    {
+        private get;
+        set;
+    } = 1f;
+
+    public NoisyLinearTransition() : base() { }
+
+    public override float getValue()
+    {
+        // Updates the transition's progression.
+        if (Duration > 0) { Progression += Time.deltaTime / Duration; }
+        else { Progression = 1; }
+
+        if (Progression >= 1) { return To; }
+
+        return From * (1 - Progression) + To * Progression + Random.Range(NoiseMin, NoiseMax);
+
+    }
+}
