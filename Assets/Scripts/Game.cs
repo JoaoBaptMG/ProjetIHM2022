20ba319a614bef36ac73c5a8053eee4e82245c20
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-10-28 21:20:32 +0200

    Added a Game class to manage the global state of the game

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,38 @@
+﻿public class Game
+{
+    // This class is a singleton
+    private static Game instance;
+
+    private Game() { }
+
+    public static Game GetInstance()
+    {
+        if (instance == null) { instance = new Game(); }
+        return instance;
+    }
+
+    // Stores the names of the level scenes in order
+    private string[] levelScenesNames = { "Level1" };
+
+    // The name of the scene holding the start menu
+    public static string StartMenuSceneName { get; } = "StartMenu";
+
+    // The name of the scene holding the menu displayed upon level completion
+    public static string LevelCompleteMenuSceneName { get; } = "LevelCompleteMenu";
+
+    // The name of the scene holding the menu displayed upon game completion
+    public static string gameCompleteMenuSceneName { get; } = "LevelCompleteMenu";
+
+    // The index of the current level's scene name in levelScenesNames
+    public int currentLevelIndex = 0;
+
+    public string GetCurrentLevelSceneName()
+    {
+        if(currentLevelIndex >= levelScenesNames.Length) { return gameCompleteMenuSceneName; }
+        if(currentLevelIndex < 0) { currentLevelIndex = 0; return StartMenuSceneName; }
+        return levelScenesNames[currentLevelIndex];
+    }
+
+    public static float Score
+    { get; set; } = 10000f;
+}
